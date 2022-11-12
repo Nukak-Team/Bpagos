ef33f660ef52b782eb5c98777c753099671211e0
JulioJavier <01juliomtz@gmail.com>
2022-11-11 20:12:22 -0500

    Se Crea ApiPagos

---
+++ index.js
@@ -0,0 +1,87 @@
+const express = require("express")
+const cors = require("cors")
+const body_parser = require("body-parser")
+const path = require("path")
+const pagoService = require("./pagoService.js")
+
+const app = express()
+const port = 8088
+
+app.use(cors())
+app.use(body_parser.json())
+
+const patName = "pagos/"
+
+app.get(patName,
+    (req, res) => {
+        console.log("Recibimos peticion");
+        console.log(req);
+
+        res.send(pagoService.pagosgetExport())
+    })
+
+app.post(patName,
+    (req, res) => {
+        console.log("Post: Recibimos las peticion");
+        console.log(req.body);
+        let pagos = pagoService.pagosSetExport(req.body)
+        res.send({"Mensaje" : "pago Guardado ","pagos":pagos})
+    })
+
+    
+app.delete(patName,
+    (req, res) => {
+        console.log("Delete: Recibimos las peticion");
+        console.log(req.body);
+        console.log("Finaliza Delete");
+        res.send("Finaliza")
+})
+
+app.put(patName,
+    (req, res) => {
+        console.log("Put: Recibimos las peticion");
+        console.log(req.body);
+        console.log("Finaliza Put");
+        res.send("Finaliza")
+})
+
+app.path(patName,
+    (req, res) => {
+        console.log("path: Recibimos las peticion");
+        console.log(req.body);
+        console.log("Finaliza path");
+        res.send("Finaliza")
+})
+
+app.listen(port,
+    () => {
+        console.log("Subio el app pago en el puerto " + port);
+})
+
+// para ApiCarrito
+// app.get(patName+"/pendientes/idcliente"){
+//     (req, res) => {
+//         console.log("Recibimos Peticion");
+//         console.log(req);
+//         idclient = req.query.id
+//         res.send(reservaCarrito.carritoPendienteIdGetExport(id))
+//     }    
+// }
+
+//Para CarritoServices.js
+// app.patch(patName+"/carrito/estado",
+//     (req, res) => {
+//         console.log("patch CarritoEstado: Recibimos las peticion");
+//         console.log(req.body);
+//         res.send(carritoServices.setEstadoCarritoExport(req.body))
+//     })
+
+// const setEstadoCarritoExport = (carritoPago) =>{
+//     for (let i = 0; i < carrito.length; i++) {
+//         if (carrito[i].id === carritoPago.idcarrito) {
+//             carrito[i].estadoPago = carritoPago.estadoCarrito
+//             i = carrito.length
+//         }
+//     }
+//     return "Carrito con pago confirmado"
+// }
---
+++ pagoService.js
@@ -0,0 +1,31 @@
+let pagos = require("./pagos.json")
+let request = require("axios")
+
+
+const pagosget = ()=>{
+    
+    return pagos
+
+}
+const pagosSet = (pago)=>{
+    if (pago.estado === "Aprobado") {
+        const carrito = request.patch(
+            "localhost:8093/carrito/estado",
+            {"idcarrito":pago.idcarrito,"estadoCarrito":"Confirmado"}  
+        ).then(
+            console.log("PAGO CONFIRMADO")
+        )            
+    }
+    pagos.push(pago)
+    return pagos
+}
+
+const pagosDelete = (id)=>{
+    pagos = pagos.filter((vuel)=>{return vuel.id != id})
+    return pagos
+
+}
+
+module.exports.pagosgetExport = pagosget;
+module.exports.pagosSetExport = pagosSet;
+module.exports.pagosDeleteExport = pagosDelete;
